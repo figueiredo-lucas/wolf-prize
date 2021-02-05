a0fbac4beff60b220be2cf481fe2233ca2863534
Lucas Figueiredo <ccomp.lucas@gmail.com>
2021-02-05 18:29:11 -0300

    wolf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import QRCode from 'easyqrcodejs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'wolf-prize';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('qrcode', { static: false }) qrcode: ElementRef;
+
+  ngAfterViewInit(): void {
+
+    // Options
+    var options = {
+      text: "Quero uma mamada",
+      logo: "assets/wolf.jpg"
+    }
+
+    // Create new QRCode Object
+    new QRCode(this.qrcode.nativeElement, options);
+  }
 }
